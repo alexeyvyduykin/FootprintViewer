af1798837f70a6b38af395319b0a74a28f8d4fe6
alexeyvyduykin <alexeyvyduykin@protonmail.com>
2021-11-12 15:06:07 +0300

    Add data selector for ToolControl

---
+++ src/FootprintViewer.WPF/Selectors/ToolIconSelector.cs
@@ -0,0 +1,39 @@
+﻿using FootprintViewer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace FootprintViewer.WPF
+{
+    public class ToolIconSelector : DataTemplateSelector
+    {
+        public DataTemplate RectangleTemplate { get; set; }
+
+        public DataTemplate PolygonTemplate { get; set; }
+
+        public DataTemplate CircleTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is Tool tool)
+            {
+                if (tool.Title == "AddRectangle")
+                {
+                    return RectangleTemplate;
+                }
+                else if (tool.Title == "AddPolygon")
+                {
+                    return PolygonTemplate;
+                }
+                else if (tool.Title == "AddCircle")
+                {
+                    return CircleTemplate;
+                }
+            }
+
+            return default;
+        }
+    }
+}
