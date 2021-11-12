48173f9bf587500950887e65539774929e0f17b5
alexeyvyduykin <alexeyvyduykin@protonmail.com>
2021-11-12 15:05:21 +0300

    Add icons for InfoPanelControl

---
+++ src/FootprintViewer.WPF/Selectors/InfoPanelIconSelector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+using FootprintViewer.ViewModels;
+
+namespace FootprintViewer.WPF
+{
+    public class InfoPanelIconSelector : DataTemplateSelector
+    {
+        public DataTemplate RouteTemplate { get; set; }
+        public DataTemplate AOITemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is InfoPanelItem infoPanelItem)
+            {
+                if (infoPanelItem.Title == "Route")
+                {
+                    return RouteTemplate;
+                }
+                else if (infoPanelItem.Title == "AOI")
+                {
+                    return AOITemplate;
+                }
+            }
+
+            return default;
+        }
+    }
+}
