3f7e3a1040bc1b1a7af5e754f19685f6567608d1
alexeyvyduykin <alexeyvyduykin@protonmail.com>
2021-12-07 11:08:45 +0300

    Set more correct cutter for features

--- src/FootprintViewer/Layers/TargetLayer.cs
+++ src/FootprintViewer/Layers/TargetLayer.cs
@@ -26,7 +26,7 @@ namespace FootprintViewer.Layers
         private BoundingBox _lastExtent = new BoundingBox(1, 1, 1, 1);
 
         public TargetLayer(IProvider provider)
-        {
+        {        
             _provider = (TargetProvider)provider;
 
             var style1 = CreateTargetHighlightThemeStyle();
@@ -36,7 +36,7 @@ namespace FootprintViewer.Layers
             Style = _style;
 
             Name = nameof(LayerType.GroundTarget);
- 
+
             DataSource = provider;
             IsMapInfoLayer = false;
         }
@@ -52,11 +52,14 @@ namespace FootprintViewer.Layers
                 if (extent.Left != _lastExtent.Left && extent.Top != _lastExtent.Top && extent.Right != _lastExtent.Right && extent.Bottom != _lastExtent.Bottom)
                 {
                     if (resolution < _maxVisible && extent.Equals(new BoundingBox(0, 0, 0, 0)) == false)
-                    {
+                    {            
+                        // HACK: change size extent to viewport of view control
+                        var box = extent.Grow(-SymbolStyle.DefaultWidth * 2 * resolution, -SymbolStyle.DefaultHeight * 2 * resolution);
+
                         OnRefreshData?.Invoke(this, new TargetLayerEventArgs()
                         {
                             Resolution = resolution,
-                            Features = GetFeaturesInView(extent, resolution),
+                            Features = GetFeaturesInView(box, resolution),
                         });
                     }
                     else
