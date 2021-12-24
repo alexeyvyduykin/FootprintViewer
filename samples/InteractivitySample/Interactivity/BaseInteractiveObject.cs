8cb62af881148ced303a61e061325f697c589730
alexeyvyduykin <alexeyvyduykin@protonmail.com>
2021-12-24 13:54:38 +0300

    Fixing feature copy for lineString

--- samples/InteractivitySample/Interactivity/BaseInteractiveObject.cs
+++ samples/InteractivitySample/Interactivity/BaseInteractiveObject.cs
@@ -17,10 +17,21 @@ namespace InteractivitySample.Interactivity
 
         protected IGeometry Copy(IGeometry geometry)
         {
-            var g = geometry.Copy();
-            var count = g.MainVertices().Count;
-            g.MainVertices().RemoveAt(count - 1);
-            return g;
+            if (geometry is LineString)
+            {
+                return geometry.Copy();
+            }
+            else if(geometry is Polygon)
+            {
+                var g = geometry.Copy();
+                var count = g.MainVertices().Count;
+                g.MainVertices().RemoveAt(count - 1);
+                return g;
+            }
+            else
+            {
+                throw new Exception();
+            }
         }
 
         public abstract void Starting(Point worldPosition);
