b29d3eafdab9341b0744b7dbb3f301b4093545dd
alexeyvyduykin <alexeyvyduykin@protonmail.com>
2022-01-31 11:50:32 +0300

    Make design time class for SidePanel

---
+++ src/FootprintViewer/Designer/DesignTimeSidePanel.cs
@@ -0,0 +1,23 @@
+﻿using FootprintViewer.ViewModels;
+using System.Collections.Generic;
+
+namespace FootprintViewer.Designer
+{
+    public class DesignTimeSidePanel : SidePanel
+    {
+        public DesignTimeSidePanel() : base()
+        {
+            var data = new DesignTimeData();
+
+            var tabs = new SidePanelTab[]
+            {
+                new SceneSearch(data),
+                new SatelliteViewer(data),
+                new GroundTargetViewer(data),
+                new FootprintObserver(data),
+            };
+
+            Tabs = new List<SidePanelTab>(tabs);
+        }
+    }
+}
--- src/FootprintViewer/Designer/DesignerContext.cs
+++ src/FootprintViewer/Designer/DesignerContext.cs
@@ -1,14 +1,11 @@
 ﻿using FootprintViewer.ViewModels;
 using Splat;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace FootprintViewer.Designer
 {
     public class DesignerContext
     {
-        public static SidePanel? SidePanel { get; private set; }
-
         public static ToolBar? ToolBar { get; private set; }
 
         public static MainViewModel? MainViewModel { get; private set; }
@@ -19,25 +16,6 @@ namespace FootprintViewer.Designer
 
             var map = dependencyResolver.GetService<Mapsui.Map>();
 
-            // SidePanel
-
-            var tabs = new SidePanelTab[]
-            {
-
-                new GroundTargetViewer(dependencyResolver)
-                {
-                    Name = "Test1",
-                    Title = "Default test title1"
-                },
-                new GroundTargetViewer(dependencyResolver)
-                {
-                    Name = "Test2",
-                    Title = "Default test title2"
-                }
-            };
-
-            SidePanel = new SidePanel() { Tabs = new List<SidePanelTab>(tabs) };
-
             // ToolBar
 
             ToolBar = dependencyResolver.GetService<ToolBar>();
@@ -46,7 +24,7 @@ namespace FootprintViewer.Designer
 
             MainViewModel = dependencyResolver.GetExistingService<MainViewModel>();
 
-            MainViewModel.SidePanel.Tabs.AddRange(new List<SidePanelTab>(tabs));
+            //     MainViewModel.SidePanel.Tabs.AddRange(new List<SidePanelTab>(tabs));
         }
     }
 
