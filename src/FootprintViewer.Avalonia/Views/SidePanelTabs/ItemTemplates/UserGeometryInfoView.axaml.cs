03fba7b84820d24c970e2d818abf6864a07160c0
alexeyvyduykin <alexeyvyduykin@protonmail.com>
2022-02-01 11:09:07 +0300

    Add remove button to UserGeometryInfoView item template

--- src/FootprintViewer.Avalonia/Views/SidePanelTabs/ItemTemplates/UserGeometryInfoView.axaml.cs
+++ src/FootprintViewer.Avalonia/Views/SidePanelTabs/ItemTemplates/UserGeometryInfoView.axaml.cs
@@ -3,7 +3,11 @@ using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using FootprintViewer.ViewModels;
 using ReactiveUI;
+using Splat;
+using System;
+using System.Reactive;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 
 namespace FootprintViewer.Avalonia.Views.SidePanelTabs.ItemTemplates
 {
@@ -13,16 +17,33 @@ namespace FootprintViewer.Avalonia.Views.SidePanelTabs.ItemTemplates
 
         private TextBlock NameTextBlock => this.FindControl<TextBlock>("NameTextBlock");
 
+        private Button RemoveButton => this.FindControl<Button>("RemoveButton");
+
+        private UserGeometryViewer? _userGeometryViewer;
+
         public UserGeometryInfoView()
         {
             InitializeComponent();
 
+            _click = ReactiveCommand.Create(ClickImpl);
+
             this.WhenActivated(disposables =>
             {
+                this.RemoveButton.Events().Click.Select(args => Unit.Default).InvokeCommand(this, v => v._click).DisposeWith(disposables);
+
                 this.OneWayBind(ViewModel, vm => vm.Type, v => v.TypeTextBlock.Text, value => ((Data.UserGeometryType)value).ToString()).DisposeWith(disposables);
 
                 this.OneWayBind(ViewModel, vm => vm.Name, v => v.NameTextBlock.Text).DisposeWith(disposables);
             });
+        }
+
+        private readonly ReactiveCommand<Unit, Unit> _click;
+
+        private void ClickImpl()
+        {
+            _userGeometryViewer ??= Locator.Current.GetExistingService<UserGeometryViewer>();
+
+            _userGeometryViewer.Remove.Execute(ViewModel).Subscribe();
         }
 
         private void InitializeComponent()
