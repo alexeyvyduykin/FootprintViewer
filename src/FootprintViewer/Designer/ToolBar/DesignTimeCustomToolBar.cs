6f6402364f4e55e8f9c08234a9f3a5378a9fb597
alexeyvyduykin <alexeyvyduykin@protonmail.com>
2022-02-03 10:10:36 +0300

    Add to CustomToolBar map selector tool

--- src/FootprintViewer/Designer/ToolBar/DesignTimeCustomToolBar.cs
+++ src/FootprintViewer/Designer/ToolBar/DesignTimeCustomToolBar.cs
@@ -1,150 +1,151 @@
 ﻿using FootprintViewer.Models;
+using FootprintViewer.ViewModels;
 using FootprintViewer.ViewModels.ToolBars;
 using System;
 
 namespace FootprintViewer.Designer
 {
-    public class DesignTimeCustomToolBar : ToolBar
+    public class DesignTimeCustomToolBar : CustomToolBar
     {
-        public DesignTimeCustomToolBar() : base()
+        public DesignTimeCustomToolBar() : base(new DesignTimeData())
         {
-            ZoomIn = new ToolClick()
-            {
-                Title = "ZoomIn",
-                Tooltip = "Приблизить",
-            };
+            //ZoomIn = new ToolClick()
+            //{
+            //    Title = "ZoomIn",
+            //    Tooltip = "Приблизить",
+            //};
 
-            ZoomOut = new ToolClick()
-            {
-                Title = "ZoomOut",
-                Tooltip = "Отдалить",
-            };
+            //ZoomOut = new ToolClick()
+            //{
+            //    Title = "ZoomOut",
+            //    Tooltip = "Отдалить",
+            //};
 
-            var addRectangle = new ToolCheck()
-            {
-                Title = "AddRectangle",
-                Tooltip = "Нарисуйте прямоугольную AOI",
-                Group = "Group1",
-            };
+            //var addRectangle = new ToolCheck()
+            //{
+            //    Title = "AddRectangle",
+            //    Tooltip = "Нарисуйте прямоугольную AOI",
+            //    Group = "Group1",
+            //};
 
-            var addPolygon = new ToolCheck()
-            {
-                Title = "AddPolygon",
-                Tooltip = "Нарисуйте полигональную AOI",
-                Group = "Group1",
-            };
+            //var addPolygon = new ToolCheck()
+            //{
+            //    Title = "AddPolygon",
+            //    Tooltip = "Нарисуйте полигональную AOI",
+            //    Group = "Group1",
+            //};
 
-            var addCircle = new ToolCheck()
-            {
-                Title = "AddCircle",
-                Tooltip = "Нарисуйте круговую AOI",
-                Group = "Group1",
-            };
+            //var addCircle = new ToolCheck()
+            //{
+            //    Title = "AddCircle",
+            //    Tooltip = "Нарисуйте круговую AOI",
+            //    Group = "Group1",
+            //};
 
-            AOICollection = new ToolCollection();
-            AOICollection.AddItem(addRectangle);
-            AOICollection.AddItem(addPolygon);
-            AOICollection.AddItem(addCircle);
+            //AOICollection = new ToolCollection();
+            //AOICollection.AddItem(addRectangle);
+            //AOICollection.AddItem(addPolygon);
+            //AOICollection.AddItem(addCircle);
 
-            RouteDistance = new ToolCheck()
-            {
-                Title = "Route",
-                Tooltip = "Измерить расстояние",
-                Group = "Group1",
-            };
+            //RouteDistance = new ToolCheck()
+            //{
+            //    Title = "Route",
+            //    Tooltip = "Измерить расстояние",
+            //    Group = "Group1",
+            //};
 
-            SelectGeometry = new ToolCheck()
-            {
-                Title = "Select",
-                Tooltip = "SelectGeometry",
-                Group = "Group2",
-            };
+            //SelectGeometry = new ToolCheck()
+            //{
+            //    Title = "Select",
+            //    Tooltip = "SelectGeometry",
+            //    Group = "Group2",
+            //};
 
-            var rectangle = new ToolCheck()
-            {
-                Title = "Rectangle",
-                Tooltip = "RectangleGeometry",
-                Group = "Group2",
-            };
+            //var rectangle = new ToolCheck()
+            //{
+            //    Title = "Rectangle",
+            //    Tooltip = "RectangleGeometry",
+            //    Group = "Group2",
+            //};
 
-            var circle = new ToolCheck()
-            {
-                Title = "Circle",
-                Tooltip = "CircleGeometry",
-                Group = "Group2",
-            };
+            //var circle = new ToolCheck()
+            //{
+            //    Title = "Circle",
+            //    Tooltip = "CircleGeometry",
+            //    Group = "Group2",
+            //};
 
-            var polygon = new ToolCheck()
-            {
-                Title = "Polygon",
-                Tooltip = "PolygonGeometry",
-                Group = "Group2",
-            };
+            //var polygon = new ToolCheck()
+            //{
+            //    Title = "Polygon",
+            //    Tooltip = "PolygonGeometry",
+            //    Group = "Group2",
+            //};
 
-            GeometryCollection = new ToolCollection();
-            GeometryCollection.AddItem(rectangle);
-            GeometryCollection.AddItem(circle);
-            GeometryCollection.AddItem(polygon);
+            //GeometryCollection = new ToolCollection();
+            //GeometryCollection.AddItem(rectangle);
+            //GeometryCollection.AddItem(circle);
+            //GeometryCollection.AddItem(polygon);
 
-            TranslateGeometry = new ToolCheck()
-            {
-                Title = "Translate",
-                Tooltip = "TranslateGeometry",
-                Group = "Group2",
-            };
+            //TranslateGeometry = new ToolCheck()
+            //{
+            //    Title = "Translate",
+            //    Tooltip = "TranslateGeometry",
+            //    Group = "Group2",
+            //};
 
-            RotateGeometry = new ToolCheck()
-            {
-                Title = "Rotate",
-                Tooltip = "RotateGeometry",
-                Group = "Group2",
-            };
+            //RotateGeometry = new ToolCheck()
+            //{
+            //    Title = "Rotate",
+            //    Tooltip = "RotateGeometry",
+            //    Group = "Group2",
+            //};
 
-            ScaleGeometry = new ToolCheck()
-            {
-                Title = "Scale",
-                Tooltip = "ScaleGeometry",
-                Group = "Group2",
-            };
+            //ScaleGeometry = new ToolCheck()
+            //{
+            //    Title = "Scale",
+            //    Tooltip = "ScaleGeometry",
+            //    Group = "Group2",
+            //};
 
-            EditGeometry = new ToolCheck()
-            {
-                Title = "Edit",
-                Tooltip = "EditGeometry",
-                Group = "Group2",
-            };
+            //EditGeometry = new ToolCheck()
+            //{
+            //    Title = "Edit",
+            //    Tooltip = "EditGeometry",
+            //    Group = "Group2",
+            //};
 
-            AddTool(ZoomIn);
-            AddTool(ZoomOut);
-            AddTool(AOICollection);
-            AddTool(RouteDistance);
-            AddTool(SelectGeometry);
-            AddTool(GeometryCollection);
-            AddTool(TranslateGeometry);
-            AddTool(RotateGeometry);
-            AddTool(ScaleGeometry);
-            AddTool(EditGeometry);
+            //AddTool(ZoomIn);
+            //AddTool(ZoomOut);
+            //AddTool(AOICollection);
+            //AddTool(RouteDistance);
+            //AddTool(SelectGeometry);
+            //AddTool(GeometryCollection);
+            //AddTool(TranslateGeometry);
+            //AddTool(RotateGeometry);
+            //AddTool(ScaleGeometry);
+            //AddTool(EditGeometry);
         }
 
-        public ToolClick ZoomIn { get; }
+        //public ToolClick ZoomIn { get; }
 
-        public ToolClick ZoomOut { get; }
+        //public ToolClick ZoomOut { get; }
 
-        public IToolCollection AOICollection { get; }
+        //public IToolCollection AOICollection { get; }
 
-        public ToolCheck RouteDistance { get; }
+        //public ToolCheck RouteDistance { get; }
 
-        public ToolCheck SelectGeometry { get; }
+        //public ToolCheck SelectGeometry { get; }
 
-        public IToolCollection GeometryCollection { get; }
+        //public IToolCollection GeometryCollection { get; }
 
-        public ToolCheck TranslateGeometry { get; }
+        //public ToolCheck TranslateGeometry { get; }
 
-        public ToolCheck RotateGeometry { get; }
+        //public ToolCheck RotateGeometry { get; }
 
-        public ToolCheck ScaleGeometry { get; }
+        //public ToolCheck ScaleGeometry { get; }
 
-        public ToolCheck EditGeometry { get; }
+        //public ToolCheck EditGeometry { get; }
 
     }
 }
