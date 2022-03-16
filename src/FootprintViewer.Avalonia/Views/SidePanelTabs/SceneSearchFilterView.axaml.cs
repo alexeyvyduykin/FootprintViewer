909f62e0e60f2166dcb4beb77d16b1b87d7e5688
alexeyvyduykin <alexeyvyduykin@protonmail.com>
2022-03-16 16:13:11 +0300

    Update SceneSearchFilter style to Material

--- src/FootprintViewer.Avalonia/Views/SidePanelTabs/SceneSearchFilterView.axaml.cs
+++ src/FootprintViewer.Avalonia/Views/SidePanelTabs/SceneSearchFilterView.axaml.cs
@@ -3,14 +3,32 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using FootprintViewer.ViewModels;
+using ReactiveUI;
+using System;
+using System.Reactive.Disposables;
 
 namespace FootprintViewer.Avalonia.Views.SidePanelTabs
 {
     public partial class SceneSearchFilterView : ReactiveUserControl<SceneSearchFilter>
     {
+        private DatePicker FromDatePicker => this.FindControl<DatePicker>("FromDatePicker");
+
+        private DatePicker ToDatePicker => this.FindControl<DatePicker>("ToDatePicker");
+
         public SceneSearchFilterView()
         {
             InitializeComponent();
+
+            this.WhenActivated(disposables =>
+            {
+                this.Bind(ViewModel, vm => vm.FromDate, v => v.FromDatePicker.SelectedDate,
+                    value => new DateTimeOffset(value),
+                    value => ((DateTimeOffset)value!).DateTime).DisposeWith(disposables);
+
+                this.Bind(ViewModel, vm => vm.ToDate, v => v.ToDatePicker.SelectedDate,
+                    value => new DateTimeOffset(value),
+                    value => ((DateTimeOffset)value!).DateTime).DisposeWith(disposables);
+            });
         }
 
         private void InitializeComponent()
