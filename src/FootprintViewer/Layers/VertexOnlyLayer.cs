9d9b8150fe63bc640b9229f17fa90a6e2a987020
alexeyvyduykin <alexeyvyduykin@protonmail.com>
2022-03-30 17:27:04 +0300

    Update VertexOnlyLayer

--- src/FootprintViewer/Layers/VertexOnlyLayer.cs
+++ src/FootprintViewer/Layers/VertexOnlyLayer.cs
@@ -1,6 +1,4 @@
-﻿using Mapsui;
-using Mapsui.Fetcher;
-using Mapsui.Geometries;
+﻿using Mapsui.Geometries;
 using Mapsui.Layers;
 using Mapsui.Providers;
 using System;
@@ -9,21 +7,14 @@ using System.Linq;
 
 namespace FootprintViewer.Layers
 {
-    public class VertexOnlyLayer : BaseLayer
+    public class VertexOnlyLayer : BaseCustomLayer
     {
-        private readonly ILayer _source;
-       
-        public override BoundingBox Envelope => _source.Envelope;
-
-        public VertexOnlyLayer(ILayer source)
-        {
-            _source = source;
-            _source.DataChanged += (sender, args) => OnDataChanged(args);    
-        }
+        public VertexOnlyLayer(ILayer source) : base(source) { }
 
         public override IEnumerable<IFeature> GetFeaturesInView(BoundingBox box, double resolution)
         {
-            var features = _source.GetFeaturesInView(box, resolution).ToList();
+            var features = Source.GetFeaturesInView(box, resolution).ToList();
+
             foreach (var feature in features)
             {
                 if (feature.Geometry is Point || feature.Geometry is MultiPoint)
@@ -34,16 +25,49 @@ namespace FootprintViewer.Layers
                 if (feature is InteractiveFeature interactiveFeature)
                 {
                     foreach (var point in interactiveFeature.EditVertices())
-                    {                          
+                    {
                         yield return new Feature { Geometry = point };
                     }
                 }
             }
         }
-
-        public override void RefreshData(BoundingBox extent, double resolution, ChangeType changeType)
-        {
-            OnDataChanged(new DataChangedEventArgs());
-        }
     }
+
+    //public class VertexOnlyLayer : BaseLayer
+    //{
+    //    private readonly ILayer _source;
+
+    //    public override BoundingBox Envelope => _source.Envelope;
+
+    //    public VertexOnlyLayer(ILayer source)
+    //    {
+    //        _source = source;
+    //        _source.DataChanged += (sender, args) => OnDataChanged(args);
+    //    }
+
+    //    public override IEnumerable<IFeature> GetFeaturesInView(BoundingBox box, double resolution)
+    //    {
+    //        var features = _source.GetFeaturesInView(box, resolution).ToList();
+    //        foreach (var feature in features)
+    //        {
+    //            if (feature.Geometry is Point || feature.Geometry is MultiPoint)
+    //            {
+    //                throw new Exception();
+    //            }
+
+    //            if (feature is InteractiveFeature interactiveFeature)
+    //            {
+    //                foreach (var point in interactiveFeature.EditVertices())
+    //                {
+    //                    yield return new Feature { Geometry = point };
+    //                }
+    //            }
+    //        }
+    //    }
+
+    //    public override void RefreshData(BoundingBox extent, double resolution, ChangeType changeType)
+    //    {
+    //        OnDataChanged(new DataChangedEventArgs());
+    //    }
+    //}
 }
