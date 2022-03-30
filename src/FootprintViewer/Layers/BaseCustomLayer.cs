350efb261c8c419599988e78ea205f1cc8121153
alexeyvyduykin <alexeyvyduykin@protonmail.com>
2022-03-30 17:03:39 +0300

    Add BaseCustomLayer

---
+++ src/FootprintViewer/Layers/BaseCustomLayer.cs
@@ -0,0 +1,35 @@
+﻿using Mapsui;
+using Mapsui.Fetcher;
+using Mapsui.Geometries;
+using Mapsui.Layers;
+using Mapsui.Providers;
+using System.Collections.Generic;
+
+namespace FootprintViewer.Layers
+{
+    public class BaseCustomLayer : BaseLayer
+    {
+        private readonly ILayer _source;
+
+        public BaseCustomLayer(ILayer source)
+        {
+            _source = source;
+            _source.DataChanged += (sender, args) => OnDataChanged(args);
+        }
+
+        public int MaxVisiblePreview { get; set; }
+
+        public override IEnumerable<IFeature> GetFeaturesInView(BoundingBox box, double resolution)
+        {
+            foreach (var feature in _source.GetFeaturesInView(box, resolution))
+            {
+                yield return feature;
+            }
+        }
+
+        public override void RefreshData(BoundingBox extent, double resolution, ChangeType changeType)
+        {
+            OnDataChanged(new DataChangedEventArgs());
+        }
+    }
+}
