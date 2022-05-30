28ead56058a524376b3d7d0252a2b16e48b16597
alexeyvyduykin <alexeyvyduykin@protonmail.com>
2022-05-30 09:27:28 +0300

    Add unique name halper

---
+++ src/FootprintViewer/Helpers/UniqueNameHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FootprintViewer.Helpers
+{
+    public static class UniqueNameHelper
+    {
+        public static string Create(string name, string ext)
+        {
+            string date = DateTime.Now.ToString("yyyy-MM-ddTHHmmss");
+
+            return string.Format("{0}.{1}.{2}", name, date, ext);
+        }
+    }
+}
