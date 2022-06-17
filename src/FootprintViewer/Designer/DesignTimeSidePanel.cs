6951cccb12a1f0661ba61909f1963bc187d93356
alexeyvyduykin <alexeyvyduykin@protonmail.com>
2022-06-17 09:27:11 +0300

    Add to side panel bottom tabs

--- src/FootprintViewer/Designer/DesignTimeSidePanel.cs
+++ src/FootprintViewer/Designer/DesignTimeSidePanel.cs
@@ -17,6 +17,7 @@ namespace FootprintViewer.Designer
                 new GroundTargetViewer(data),
                 new FootprintObserver(data),
                 new UserGeometryViewer(data),
+                new AppSettings(),
             };
 
             Tabs.AddRange(new List<SidePanelTab>(tabs));
