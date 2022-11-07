1db42abb4c1a2ea17789b9b945c185ee0ba033ae
Alexey Vyduykin <alexeyvyduykin@protonmail.com>
2022-11-07 09:09:46 +0300

    Create json data builder sample

---
+++ samples/JsonDataBuilderSample/DataBuilder.cs
@@ -0,0 +1,53 @@
+﻿using FootprintViewer.Data;
+using FootprintViewer.Data.DataManager.Sources;
+
+namespace JsonDataBuilderSample
+{
+    public class DataBuilder
+    {
+        public DataBuilder()
+        {
+
+        }
+
+        public static async Task<IList<Footprint>> CreateRandomFootprints(int count)
+        {
+            var footprintSource = new FootprintRandomSource()
+            {
+                GenerateCount = count
+            };
+
+            return (IList<Footprint>)await footprintSource.GetValuesAsync();
+        }
+
+        public static async Task<IList<GroundTarget>> CreateRandomGroundTargets(int count)
+        {
+            var groundTargetsSource = new GroundTargetRandomSource()
+            {
+                GenerateCount = count
+            };
+
+            return (IList<GroundTarget>)await groundTargetsSource.GetValuesAsync();
+        }
+
+        public static async Task<IList<Satellite>> CreateRandomSatellites(int count)
+        {
+            var satellitesSource = new SatelliteRandomSource()
+            {
+                GenerateCount = count
+            };
+
+            return (IList<Satellite>)await satellitesSource.GetValuesAsync();
+        }
+
+        public static async Task<IList<Satellite>> CreateRandomGroundStations(int count)
+        {
+            var groundStationsSource = new GroundStationRandomSource()
+            {
+                GenerateCount = (count > 6) ? 6 : count
+            };
+
+            return (IList<Satellite>)await groundStationsSource.GetValuesAsync();
+        }
+    }
+}
