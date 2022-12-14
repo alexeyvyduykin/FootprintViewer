31267b04ecb7d17faa3f8c1732ebadc974f77489
Alexey Vyduykin <alexeyvyduykin@protonmail.com>
2022-12-14 16:57:10 +0300

    Remove data sources from settings

--- src/FootprintViewer/ViewModels/Settings/SettingsViewModel.cs
+++ src/FootprintViewer/ViewModels/Settings/SettingsViewModel.cs
@@ -6,8 +6,6 @@ using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using Splat;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace FootprintViewer.ViewModels.Settings;
@@ -21,67 +19,11 @@ public class SettingsViewModel : DialogViewModelBase<object>
         _dataManager = dependencyResolver.GetExistingService<IDataManager>();
         var languageManager = dependencyResolver.GetExistingService<ILanguageManager>();
 
-        int counter = 0;
-
-        var footprintsSources = _dataManager.GetSources(DbKeys.Footprints.ToString());
-        var groundTargetsSources = _dataManager.GetSources(DbKeys.GroundTargets.ToString());
-        var satellitesSources = _dataManager.GetSources(DbKeys.Satellites.ToString());
-        var groundStationsSources = _dataManager.GetSources(DbKeys.GroundStations.ToString());
-        var userGeometriesSources = _dataManager.GetSources(DbKeys.UserGeometries.ToString());
-
-        SourceContainers = new List<SourceContainerViewModel>()
-        {
-            new SourceContainerViewModel(DbKeys.Footprints.ToString(), dependencyResolver)
-            {
-                Header = DbKeys.Footprints.ToString(),
-                Sources = footprintsSources.Select(s => new SourceViewModel(s) { Name = $"Source{++counter}" }).ToList<ISourceViewModel>(),
-            },
-            new SourceContainerViewModel(DbKeys.GroundTargets.ToString(), dependencyResolver)
-            {
-                Header = DbKeys.GroundTargets.ToString(),
-                Sources = groundTargetsSources.Select(s => new SourceViewModel(s) { Name = $"Source{++counter}" } ).ToList<ISourceViewModel>(),
-            },
-            new SourceContainerViewModel(DbKeys.Satellites.ToString(), dependencyResolver)
-            {
-                Header = DbKeys.Satellites.ToString(),
-                Sources = satellitesSources.Select(s => new SourceViewModel(s) { Name = $"Source{++counter}" } ).ToList<ISourceViewModel>(),
-            },
-            new SourceContainerViewModel(DbKeys.GroundStations.ToString(), dependencyResolver)
-            {
-                Header = DbKeys.GroundStations.ToString(),
-                Sources = groundStationsSources.Select(s => new SourceViewModel(s) { Name = $"Source{++counter}" } ).ToList<ISourceViewModel>(),
-            },
-            new SourceContainerViewModel(DbKeys.UserGeometries.ToString(), dependencyResolver)
-            {
-                Header = DbKeys.UserGeometries.ToString(),
-                Sources = userGeometriesSources.Select(s => new SourceViewModel(s) { Name = $"Source{++counter}" } ).ToList<ISourceViewModel>(),
-            },
-        };
-
         NextCommand = ReactiveCommand.CreateFromTask(async () =>
         {
             var mainState = dependencyResolver.GetExistingService<MainState>();
 
             await Task.Delay(TimeSpan.FromSeconds(0.1));
-
-            foreach (var container in SourceContainers)
-            {
-                _ = Enum.TryParse<DbKeys>(container.Header, out var key);
-
-                foreach (var (source, op) in container.SourceOperationsStack.Reverse())
-                {
-                    // add
-                    if (op == true)
-                    {
-                        _dataManager.RegisterSource(key.ToString(), source);
-                    }
-                    // remove
-                    else if (op == false)
-                    {
-                        _dataManager.UnregisterSource(key.ToString(), source);
-                    }
-                }
-            }
 
             mainState.SaveData(_dataManager);
 
@@ -92,12 +34,6 @@ public class SettingsViewModel : DialogViewModelBase<object>
 
         LanguageSettings.Activate();
     }
-
-    [Reactive]
-    public IList<SourceContainerViewModel> SourceContainers { get; set; }
-
-    [Reactive]
-    public SourceContainerViewModel? SelectedSourceContainer { get; set; }
 
     [Reactive]
     public LanguageSettingsViewModel LanguageSettings { get; set; }
