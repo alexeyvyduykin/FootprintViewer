8376232c12e801b8a6b55334235c89365db72d84
Alexey Vyduykin <alexeyvyduykin@protonmail.com>
2023-01-24 14:58:02 +0300

    Create PlannedSchedule json data

---
+++ samples/JsonDataBuilderSample/PlannedSchedule/PlannedScheduleBuilder.cs
@@ -0,0 +1,83 @@
+﻿using FootprintViewer.Data;
+using FootprintViewer.Data.Models;
+
+namespace JsonDataBuilderSample;
+
+public static class PlannedScheduleBuilder
+{
+    public static PlannedScheduleResult Build(IList<Satellite> satellites, IList<GroundTarget> groundTargets, IList<GroundStation> groundStations, IList<Footprint> footprints)
+    {
+        var observationTasks = groundTargets.Select((s, index) => new ObservationTask()
+        {
+            Name = $"ObservationTask_{index + 1}",
+            TargetName = s.Name ?? string.Empty
+        }).OfType<ITask>().ToList();
+
+        var communicationTasks = groundStations.Select((s, index) => new CommunicationTask()
+        {
+            Name = $"CommunicationTask_{index + 1}",
+            GroundTargetName = s.Name ?? string.Empty
+        }).OfType<ITask>().ToList();
+
+        var tasks = observationTasks.Concat(communicationTasks).ToList();
+
+        var plannedScheduleItems = satellites.ToDictionary(s => s.Name ?? string.Empty, _ => new List<PlannedScheduleItem>());
+        var plannedSchedules = satellites.ToDictionary(s => s.Name ?? string.Empty, _ => new PlannedSchedule());
+
+        foreach (var item in footprints)
+        {
+            var satelliteName = item.SatelliteName ?? string.Empty;
+            var targetName = item.TargetName;
+            var interval = new Interval()
+            {
+                Begin = item.Begin,
+                Duration = item.Duration
+            };
+
+            var task = tasks
+                .Where(s => s is ObservationTask observationTask && Equals(observationTask.TargetName, targetName))
+                .FirstOrDefault();
+
+            var plannedScheduleItem = new PlannedScheduleItem()
+            {
+                TaskName = task?.Name ?? string.Empty,
+                Interval = interval,
+            };
+
+            plannedScheduleItems[satelliteName].Add(plannedScheduleItem);
+        }
+
+        foreach (var item in satellites)
+        {
+            var satelliteName = item.Name ?? string.Empty;
+
+            var satellitePlannedSchedule = plannedScheduleItems[satelliteName];
+
+            string? Func(IList<ITask>? tasks, string targetName)
+            {
+                return tasks?
+                    .Where(s => s is ObservationTask observationTask && Equals(observationTask.TargetName, targetName))
+                    .FirstOrDefault()?.Name;
+            }
+
+            var fs = footprints.Select(s => new FootprintFrame()
+            {
+                TaskName = Func(tasks, s.TargetName ?? string.Empty) ?? string.Empty,
+                Center = s.Center ?? new(null),
+                Points = s.Points ?? new(null)
+            }).ToList();
+
+            plannedSchedules[satelliteName] = new PlannedSchedule()
+            {
+                Items = plannedScheduleItems[satelliteName],
+                Footprints = fs
+            };
+        }
+
+        return new PlannedScheduleResult()
+        {
+            Tasks = tasks,
+            PlannedSchedules = plannedSchedules
+        };
+    }
+}
--- samples/JsonDataBuilderSample/Program.cs
+++ samples/JsonDataBuilderSample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using FootprintViewer.Data;
+using FootprintViewer.Data.Models;
 using System.Reflection;
 
 namespace JsonDataBuilderSample;
@@ -13,26 +14,32 @@ internal class Program
         var satellitePath = Path.GetFullPath(Path.Combine(root, @"..\..\..\Output", "Satellites.json"));
         var groundTargetPath = Path.GetFullPath(Path.Combine(root, @"..\..\..\Output", "GroundTargets.json"));
         var groundStationPath = Path.GetFullPath(Path.Combine(root, @"..\..\..\Output", "GroundStations.json"));
+        var plannedSchedulePath = Path.GetFullPath(Path.Combine(root, @"..\..\..\Output", "PlannedSchedule.json"));
 
         var satellites = await DataBuilder.CreateRandomSatellites(5);
         var groundStations = await DataBuilder.CreateRandomGroundStations(6);
         var footprints = await DataBuilder.CreateRandomFootprints(satellites, 2000);
         var groundTargets = await DataBuilder.CreateRandomGroundTargets(footprints, 5000);
+        var plannedSchedule = PlannedScheduleBuilder.Build(satellites, groundTargets, groundStations, footprints);
 
-        JsonHelper.SerializeToFile(footprintPath, footprints);
+    //    JsonHelper.SerializeToFile(footprintPath, footprints);
         var footprintSuccess = JsonHelper.Verified<IList<Footprint>>(footprintPath);
         Console.WriteLine($"File {Path.GetFileName(footprintPath)} is build and verified = {footprintSuccess}.");
 
-        JsonHelper.SerializeToFile(satellitePath, satellites);
+    //    JsonHelper.SerializeToFile(satellitePath, satellites);
         var satelliteSuccess = JsonHelper.Verified<IList<Satellite>>(satellitePath);
         Console.WriteLine($"File {Path.GetFileName(satellitePath)} is build and verified = {satelliteSuccess}.");
 
-        JsonHelper.SerializeToFile(groundStationPath, groundStations);
+    //    JsonHelper.SerializeToFile(groundStationPath, groundStations);
         var groundStationSuccess = JsonHelper.Verified<IList<GroundStation>>(groundStationPath);
         Console.WriteLine($"File {Path.GetFileName(groundStationPath)} is build and verified = {groundStationSuccess}.");
 
-        JsonHelper.SerializeToFile(groundTargetPath, groundTargets);
+    //    JsonHelper.SerializeToFile(groundTargetPath, groundTargets);
         var groundTargetSuccess = JsonHelper.Verified<IList<GroundTarget>>(groundTargetPath);
         Console.WriteLine($"File {Path.GetFileName(groundTargetPath)} is build and verified = {groundTargetSuccess}.");
+
+        JsonHelper.SerializeToFile(plannedSchedulePath, plannedSchedule);
+        var plannedScheduleSuccess = JsonHelper.Verified<PlannedScheduleResult>(plannedSchedulePath);
+        Console.WriteLine($"File {Path.GetFileName(plannedSchedulePath)} is build and verified = {plannedScheduleSuccess}.");
     }
 }
