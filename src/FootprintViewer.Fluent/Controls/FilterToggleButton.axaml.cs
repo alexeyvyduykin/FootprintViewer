91b11887f143e7da72be36c0d463058f084ef490
Alexey Vyduykin <alexeyvyduykin@protonmail.com>
2023-04-26 13:50:41 +0300

    Add FilterToggleButton control

---
+++ src/FootprintViewer.Fluent/Controls/FilterToggleButton.axaml.cs
@@ -0,0 +1,93 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using System.Windows.Input;
+
+namespace FootprintViewer.Fluent.Controls
+{
+    public partial class FilterToggleButton : ToggleButton
+    {
+        private ICommand? _resetCommand;
+
+        public static readonly StyledProperty<string> TitleProperty =
+            AvaloniaProperty.Register<FilterToggleButton, string>(nameof(Title));
+
+        public string Title
+        {
+            get => GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly StyledProperty<Flyout?> FilterProperty =
+            AvaloniaProperty.Register<FilterToggleButton, Flyout?>(nameof(Filter));
+
+        public Flyout? Filter
+        {
+            get => GetValue(FilterProperty);
+            set => SetValue(FilterProperty, value);
+        }
+
+        public static readonly StyledProperty<FlyoutPlacementMode> PlacementProperty =
+            AvaloniaProperty.Register<FilterToggleButton, FlyoutPlacementMode>(nameof(Placement), FlyoutPlacementMode.LeftEdgeAlignedTop);
+
+        public FlyoutPlacementMode Placement
+        {
+            get => GetValue(PlacementProperty);
+            set => SetValue(PlacementProperty, value);
+        }
+
+        public static readonly StyledProperty<bool> IsDirtyProperty =
+            AvaloniaProperty.Register<FilterToggleButton, bool>(nameof(IsDirty), false);
+
+        public bool IsDirty
+        {
+            get => GetValue(IsDirtyProperty);
+            set => SetValue(IsDirtyProperty, value);
+        }
+
+        public static readonly DirectProperty<FilterToggleButton, ICommand?> ResetCommandProperty =
+            AvaloniaProperty.RegisterDirect<FilterToggleButton, ICommand?>(nameof(ResetCommand), s => s.ResetCommand, (s, command) => s.ResetCommand = command, enableDataValidation: true);
+
+        public ICommand? ResetCommand
+        {
+            get { return _resetCommand; }
+            set { SetAndRaise(ResetCommandProperty, ref _resetCommand, value); }
+        }
+
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            ClickMode = ClickMode.Press;
+
+            Flyout = Filter;
+
+            base.OnApplyTemplate(e);
+        }
+
+        protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> e)
+        {
+            base.OnPropertyChanged(e);
+
+            if (e.Property == FilterProperty)
+            {
+                if (Filter is { })
+                {
+                    Filter.Placement = Placement;
+                    // TODO: ???                 
+                    Filter.Closed += Flyout_Closed;
+                }
+            }
+            else if (e.Property == PlacementProperty)
+            {
+                if (Filter is { })
+                {
+                    Filter.Placement = Placement;
+                }
+            }
+        }
+
+        private void Flyout_Closed(object? sender, System.EventArgs e)
+        {
+            IsChecked = false;
+        }
+    }
+}
--- src/FootprintViewer.Fluent/Views/SidePanel/Tabs/FootprintTabView.axaml.cs
+++ src/FootprintViewer.Fluent/Views/SidePanel/Tabs/FootprintTabView.axaml.cs
@@ -7,13 +7,6 @@ namespace FootprintViewer.Fluent.Views.SidePanel.Tabs
         public FootprintTabView()
         {
             InitializeComponent();
-
-            SearchToggleButton.Flyout.Closed += Flyout_Closed;
-        }
-
-        private void Flyout_Closed(object? sender, System.EventArgs e)
-        {
-            SearchToggleButton.IsChecked = false;
         }
     }
 }
