5f537523cbd58aedf0c685144ed8541667549030
Alexey Vyduykin <alexeyvyduykin@protonmail.com>
2023-05-02 15:46:44 +0300

    Add range slider

---
+++ src/FootprintViewer.Fluent/Controls/RangeBase.cs
@@ -0,0 +1,203 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using Avalonia.Utilities;
+
+namespace FootprintViewer.Fluent.Controls;
+
+public abstract class RangeBase : TemplatedControl
+{
+    public static readonly DirectProperty<RangeBase, double> MinimumProperty =
+        AvaloniaProperty.RegisterDirect<RangeBase, double>(
+            nameof(Minimum),
+            o => o.Minimum,
+            (o, v) => o.Minimum = v);
+
+    public static readonly DirectProperty<RangeBase, double> MaximumProperty =
+        AvaloniaProperty.RegisterDirect<RangeBase, double>(
+            nameof(Maximum),
+            o => o.Maximum,
+            (o, v) => o.Maximum = v);
+
+    public static readonly DirectProperty<RangeBase, double> LowerSelectedValueProperty =
+        AvaloniaProperty.RegisterDirect<RangeBase, double>(
+            nameof(LowerSelectedValue),
+            o => o.LowerSelectedValue,
+            (o, v) => o.LowerSelectedValue = v,
+            defaultBindingMode: BindingMode.TwoWay);
+
+    public static readonly DirectProperty<RangeBase, double> UpperSelectedValueProperty =
+        AvaloniaProperty.RegisterDirect<RangeBase, double>(
+            nameof(UpperSelectedValue),
+            o => o.UpperSelectedValue,
+            (o, v) => o.UpperSelectedValue = v,
+            defaultBindingMode: BindingMode.TwoWay);
+
+    public static readonly StyledProperty<double> SmallChangeProperty =
+        AvaloniaProperty.Register<RangeBase, double>(nameof(SmallChange), 1);
+
+    public static readonly StyledProperty<double> LargeChangeProperty =
+        AvaloniaProperty.Register<RangeBase, double>(nameof(LargeChange), 10);
+
+    private double _minimum;
+    private double _maximum = 100.0;
+    private double _lowerSelectedValue;
+    private double _upperSelectedValue;
+    private bool _upperValueInitializedNonZeroValue = false;
+
+    public RangeBase()
+    {
+    }
+
+    public double Minimum
+    {
+        get
+        {
+            return _minimum;
+        }
+
+        set
+        {
+            if (!ValidateDouble(value))
+            {
+                return;
+            }
+
+            if (IsInitialized)
+            {
+                SetAndRaise(MinimumProperty, ref _minimum, value);
+                Maximum = ValidateMaximum(Maximum);
+                LowerSelectedValue = ValidateLowerValue(LowerSelectedValue);
+                UpperSelectedValue = ValidateUpperValue(UpperSelectedValue);
+            }
+            else
+            {
+                SetAndRaise(MinimumProperty, ref _minimum, value);
+            }
+        }
+    }
+
+    public double Maximum
+    {
+        get
+        {
+            return _maximum;
+        }
+
+        set
+        {
+            if (!ValidateDouble(value))
+            {
+                return;
+            }
+
+            if (IsInitialized)
+            {
+                value = ValidateMaximum(value);
+                SetAndRaise(MaximumProperty, ref _maximum, value);
+                LowerSelectedValue = ValidateLowerValue(LowerSelectedValue);
+                UpperSelectedValue = ValidateUpperValue(UpperSelectedValue);
+            }
+            else
+            {
+                SetAndRaise(MaximumProperty, ref _maximum, value);
+            }
+        }
+    }
+
+    public double LowerSelectedValue
+    {
+        get
+        {
+            return _lowerSelectedValue;
+        }
+
+        set
+        {
+            if (!ValidateDouble(value))
+            {
+                return;
+            }
+
+            if (IsInitialized)
+            {
+                value = ValidateLowerValue(value);
+                SetAndRaise(LowerSelectedValueProperty, ref _lowerSelectedValue, value);
+            }
+            else
+            {
+                SetAndRaise(LowerSelectedValueProperty, ref _lowerSelectedValue, value);
+            }
+        }
+    }
+
+    public double UpperSelectedValue
+    {
+        get
+        {
+            return _upperSelectedValue;
+        }
+
+        set
+        {
+            if (!ValidateDouble(value))
+            {
+                return;
+            }
+
+            if (IsInitialized)
+            {
+                value = ValidateUpperValue(value);
+                _upperValueInitializedNonZeroValue = value > 0.0;
+                SetAndRaise(UpperSelectedValueProperty, ref _upperSelectedValue, value);
+            }
+            else
+            {
+                SetAndRaise(UpperSelectedValueProperty, ref _upperSelectedValue, value);
+            }
+        }
+    }
+
+    public double SmallChange
+    {
+        get => GetValue(SmallChangeProperty);
+        set => SetValue(SmallChangeProperty, value);
+    }
+
+    public double LargeChange
+    {
+        get => GetValue(LargeChangeProperty);
+        set => SetValue(LargeChangeProperty, value);
+    }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Maximum = ValidateMaximum(Maximum);
+        LowerSelectedValue = ValidateLowerValue(LowerSelectedValue);
+        UpperSelectedValue = ValidateUpperValue(UpperSelectedValue);
+    }
+
+    private static bool ValidateDouble(double value)
+    {
+        return !double.IsInfinity(value) || !double.IsNaN(value);
+    }
+
+    private double ValidateMaximum(double value)
+    {
+        return Math.Max(value, Minimum);
+    }
+
+    private double ValidateLowerValue(double value)
+    {
+        return _upperValueInitializedNonZeroValue
+            ? MathUtilities.Clamp(value, Minimum, UpperSelectedValue)
+            : MathUtilities.Clamp(value, Minimum, Maximum);
+    }
+
+    private double ValidateUpperValue(double value)
+    {
+        return MathUtilities.Clamp(value, LowerSelectedValue, Maximum);
+    }
+}
---
+++ src/FootprintViewer.Fluent/Controls/RangeSlider.axaml.cs
@@ -0,0 +1,513 @@
+﻿using Avalonia;
+using Avalonia.Collections;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Controls.Mixins;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Layout;
+using Avalonia.Utilities;
+
+namespace FootprintViewer.Fluent.Controls;
+
+[PseudoClasses(":vertical", ":horizontal", ":pressed")]
+public class RangeSlider : RangeBase
+{
+    private enum TrackThumb
+    {
+        None,
+        Upper,
+        InnerUpper,
+        OuterUpper,
+        Lower,
+        InnerLower,
+        OuterLower,
+        Both,
+        Overlapped
+    };
+
+    public static readonly StyledProperty<Orientation> OrientationProperty =
+        ScrollBar.OrientationProperty.AddOwner<RangeSlider>();
+
+    public static readonly StyledProperty<bool> IsDirectionReversedProperty =
+        RangeTrack.IsDirectionReversedProperty.AddOwner<RangeSlider>();
+
+    public static readonly StyledProperty<bool> IsThumbOverlapProperty =
+        RangeTrack.IsThumbOverlapProperty.AddOwner<RangeSlider>();
+
+    public static readonly StyledProperty<bool> IsSnapToTickEnabledProperty =
+        AvaloniaProperty.Register<RangeSlider, bool>(nameof(IsSnapToTickEnabled), false);
+
+    public static readonly StyledProperty<bool> MoveWholeRangeProperty =
+        AvaloniaProperty.Register<RangeSlider, bool>(nameof(MoveWholeRange), false);
+
+    public static readonly StyledProperty<double> TickFrequencyProperty =
+        AvaloniaProperty.Register<RangeSlider, double>(nameof(TickFrequency), 0.0);
+
+    public static readonly StyledProperty<TickPlacement> TickPlacementProperty =
+        AvaloniaProperty.Register<TickBar, TickPlacement>(nameof(TickPlacement), 0d);
+
+    public static readonly StyledProperty<AvaloniaList<double>> TicksProperty =
+        TickBar.TicksProperty.AddOwner<RangeSlider>();
+
+    private double _previousValue = 0.0;
+    private bool _isDragging = false;
+    private RangeTrack _track = null!;
+    private TrackThumb _currentTrackThumb = TrackThumb.None;
+    private const double Tolerance = 0.0001;
+
+    static RangeSlider()
+    {
+        PressedMixin.Attach<RangeSlider>();
+        FocusableProperty.OverrideDefaultValue<RangeSlider>(true);
+        OrientationProperty.OverrideDefaultValue(typeof(RangeSlider), Orientation.Horizontal);
+
+        LowerSelectedValueProperty.OverrideMetadata<RangeSlider>(new DirectPropertyMetadata<double>(enableDataValidation: true));
+        UpperSelectedValueProperty.OverrideMetadata<RangeSlider>(new DirectPropertyMetadata<double>(enableDataValidation: true));
+    }
+
+    public RangeSlider()
+    {
+        UpdatePseudoClasses(Orientation);
+    }
+
+    public AvaloniaList<double> Ticks
+    {
+        get => GetValue(TicksProperty);
+        set => SetValue(TicksProperty, value);
+    }
+
+    public Orientation Orientation
+    {
+        get { return GetValue(OrientationProperty); }
+        set { SetValue(OrientationProperty, value); }
+    }
+
+    public bool MoveWholeRange
+    {
+        get { return GetValue(MoveWholeRangeProperty); }
+        set { SetValue(MoveWholeRangeProperty, value); }
+    }
+
+    public bool IsDirectionReversed
+    {
+        get { return GetValue(IsDirectionReversedProperty); }
+        set { SetValue(IsDirectionReversedProperty, value); }
+    }
+
+    public bool IsSnapToTickEnabled
+    {
+        get { return GetValue(IsSnapToTickEnabledProperty); }
+        set { SetValue(IsSnapToTickEnabledProperty, value); }
+    }
+
+    public bool IsThumbOverlap
+    {
+        get { return GetValue(IsThumbOverlapProperty); }
+        set { SetValue(IsThumbOverlapProperty, value); }
+    }
+
+    public double TickFrequency
+    {
+        get { return GetValue(TickFrequencyProperty); }
+        set { SetValue(TickFrequencyProperty, value); }
+    }
+
+    public TickPlacement TickPlacement
+    {
+        get { return GetValue(TickPlacementProperty); }
+        set { SetValue(TickPlacementProperty, value); }
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+
+        _track = e.NameScope.Find<RangeTrack>("PART_Track");
+
+        AddHandler(PointerPressedEvent, TrackPressed, RoutingStrategies.Tunnel);
+        AddHandler(PointerMovedEvent, TrackMoved, RoutingStrategies.Tunnel);
+        AddHandler(PointerReleasedEvent, TrackReleased, RoutingStrategies.Tunnel);
+    }
+
+    protected override void OnKeyDown(KeyEventArgs e)
+    {
+        base.OnKeyDown(e);
+
+        if (e.Handled || e.KeyModifiers != KeyModifiers.None)
+        {
+            return;
+        }
+
+        var handled = true;
+
+        switch (e.Key)
+        {
+            case Key.Down:
+            case Key.Left:
+                MoveToNextTick(IsDirectionReversed ? SmallChange : -SmallChange);
+                break;
+
+            case Key.Up:
+            case Key.Right:
+                MoveToNextTick(IsDirectionReversed ? -SmallChange : SmallChange);
+                break;
+
+            case Key.PageUp:
+                MoveToNextTick(IsDirectionReversed ? -LargeChange : LargeChange);
+                break;
+
+            case Key.PageDown:
+                MoveToNextTick(IsDirectionReversed ? LargeChange : -LargeChange);
+                break;
+
+            case Key.Home:
+                LowerSelectedValue = Minimum;
+                break;
+
+            case Key.End:
+                UpperSelectedValue = Maximum;
+                break;
+
+            default:
+                handled = false;
+                break;
+        }
+
+        e.Handled = handled;
+    }
+
+    private void MoveToNextTick(double direction)
+    {
+        if (direction == 0.0)
+        {
+            return;
+        }
+
+        var value = LowerSelectedValue;
+
+        var next = SnapToTick(Math.Max(Minimum, Math.Min(Maximum, value + direction)));
+
+        var greaterThan = direction > 0;
+
+        if (Math.Abs(next - value) < Tolerance
+            && !(greaterThan && Math.Abs(value - Maximum) < Tolerance)
+            && !(!greaterThan && Math.Abs(value - Minimum) < Tolerance))
+        {
+            var ticks = Ticks;
+
+            if (ticks != null && ticks.Count > 0)
+            {
+                foreach (var tick in ticks)
+                {
+                    if (greaterThan && MathUtilities.GreaterThan(tick, value) &&
+                        (MathUtilities.LessThan(tick, next) || Math.Abs(next - value) < Tolerance)
+                        || !greaterThan && MathUtilities.LessThan(tick, value) &&
+                        (MathUtilities.GreaterThan(tick, next) || Math.Abs(next - value) < Tolerance))
+                    {
+                        next = tick;
+                    }
+                }
+            }
+            else if (MathUtilities.GreaterThan(TickFrequency, 0.0))
+            {
+                var tickNumber = Math.Round((value - Minimum) / TickFrequency);
+
+                if (greaterThan)
+                {
+                    tickNumber += 1.0;
+                }
+                else
+                {
+                    tickNumber -= 1.0;
+                }
+
+                next = Minimum + tickNumber * TickFrequency;
+            }
+        }
+
+        if (Math.Abs(next - value) > Tolerance)
+        {
+            LowerSelectedValue = next;
+        }
+    }
+
+    private void TrackPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+        {
+            _isDragging = true;
+
+            var pointerCoord = e.GetCurrentPoint(_track).Position;
+            _previousValue = GetValueByPointOnTrack(pointerCoord);
+            _currentTrackThumb = GetNearestTrackThumb(pointerCoord);
+
+            if (IsPressedOnTrackBetweenThumbs() == false && MoveWholeRange == true)
+            {
+                MoveToPoint(pointerCoord, _currentTrackThumb);
+            }
+            else if (MoveWholeRange == false && _currentTrackThumb != TrackThumb.Overlapped)
+            {
+                MoveToPoint(pointerCoord, _currentTrackThumb);
+            }
+        }
+    }
+
+    private void TrackReleased(object? sender, PointerReleasedEventArgs e)
+    {
+        _isDragging = false;
+        _currentTrackThumb = TrackThumb.None;
+    }
+
+    private void TrackMoved(object? sender, PointerEventArgs e)
+    {
+        if (_isDragging)
+        {
+            var pointerCoord = e.GetCurrentPoint(_track).Position;
+
+            if (_currentTrackThumb == TrackThumb.Overlapped)
+            {
+                SelectThumbBasedOnPointerDirection(pointerCoord);
+            }
+
+            if (IsPressedOnTrackBetweenThumbs() == false && MoveWholeRange)
+            {
+                MoveToPoint(pointerCoord, _currentTrackThumb);
+            }
+            if (IsPressedOnTrackBetweenThumbs() == true && MoveWholeRange)
+            {
+                MoveToPoint(pointerCoord, TrackThumb.Both);
+            }
+            else if (MoveWholeRange == false)
+            {
+                MoveToPoint(pointerCoord, _currentTrackThumb);
+            }
+        }
+    }
+
+    private void MoveToPoint(Point pointerCoord, TrackThumb trackThumb)
+    {
+        var value = GetValueByPointOnTrack(pointerCoord);
+
+        switch (trackThumb)
+        {
+            case TrackThumb.Upper:
+            case TrackThumb.InnerUpper:
+            case TrackThumb.OuterUpper:
+                UpperSelectedValue = SnapToTick(value);
+                break;
+            case TrackThumb.Lower:
+            case TrackThumb.InnerLower:
+            case TrackThumb.OuterLower:
+                LowerSelectedValue = SnapToTick(value);
+                break;
+            case TrackThumb.Both:
+                var delta = value - _previousValue;
+
+                if ((Math.Abs(LowerSelectedValue - Minimum) <= Tolerance && delta <= 0d)
+                    || (Math.Abs(UpperSelectedValue - Maximum) <= Tolerance && delta >= 0d))
+                {
+                    return;
+                }
+
+                if (IsSnapToTickEnabled == false)
+                {
+                    _previousValue = value;
+                    LowerSelectedValue += delta;
+                    UpperSelectedValue += delta;
+                }
+                else
+                {
+                    var closestTick = SnapToTick(Math.Abs(delta) / 2d);
+                    if (closestTick > 0d)
+                    {
+                        _previousValue = value;
+                        LowerSelectedValue += closestTick * Math.Sign(delta);
+                        UpperSelectedValue += closestTick * Math.Sign(delta);
+                    }
+                }
+                break;
+        }
+    }
+
+    private double GetValueByPointOnTrack(Point pointerCoord)
+    {
+        var orient = Orientation == Orientation.Horizontal;
+        var trackLength = orient ? _track.Bounds.Width : _track.Bounds.Height;
+        var pointNum = orient ? pointerCoord.X : pointerCoord.Y;
+        var thumbLength = orient ? _track.LowerThumb.Width : _track.LowerThumb.Height;
+
+        trackLength += double.Epsilon;
+
+        if (IsThumbOverlap)
+        {
+            thumbLength /= 2.0;
+        }
+
+        if (pointNum <= thumbLength)
+        {
+            return orient ? Minimum : Maximum;
+        }
+        if (pointNum > trackLength - thumbLength)
+        {
+            return orient ? Maximum : Minimum;
+        }
+
+        trackLength -= 2.0 * thumbLength;
+        pointNum -= thumbLength;
+
+        var logicalPos = MathUtilities.Clamp(pointNum / trackLength, 0.0d, 1.0d);
+        var invert = orient
+            ? IsDirectionReversed ? 1 : 0
+            : IsDirectionReversed ? 0 : 1;
+        var calcVal = Math.Abs(invert - logicalPos);
+        var range = Maximum - Minimum;
+        var finalValue = calcVal * range + Minimum;
+
+        return finalValue;
+    }
+
+    private bool IsPressedOnTrackBetweenThumbs()
+    {
+        return _currentTrackThumb == TrackThumb.InnerLower || _currentTrackThumb == TrackThumb.InnerUpper;
+    }
+
+    private void SelectThumbBasedOnPointerDirection(Point pointerCoord)
+    {
+        var value = GetValueByPointOnTrack(pointerCoord);
+        var delta = _previousValue - value;
+
+        if (delta >= 0d && delta < Tolerance)
+        {
+            return;
+        }
+
+        if (delta > 0d)
+        {
+            _currentTrackThumb = TrackThumb.Lower;
+        }
+        else
+        {
+            _currentTrackThumb = TrackThumb.Upper;
+        }
+    }
+
+    private TrackThumb GetNearestTrackThumb(Point pointerCoord)
+    {
+        var orient = Orientation == Orientation.Horizontal;
+
+        var lowerThumbPos = orient ? _track.LowerThumb.Bounds.Position.X : _track.LowerThumb.Bounds.Position.Y;
+        var upperThumbPos = orient ? _track.UpperThumb.Bounds.Position.X : _track.UpperThumb.Bounds.Position.Y;
+        var thumbWidth = orient ? _track.LowerThumb.Bounds.Width : _track.LowerThumb.Bounds.Height;
+        var thumbHalfWidth = thumbWidth / 2d;
+
+        var pointerPos = orient ? pointerCoord.X : pointerCoord.Y;
+
+        if (IsThumbOverlap == true)
+        {
+            var isThumbsOverlapped = Math.Abs(lowerThumbPos - upperThumbPos) < Tolerance;
+
+            if (isThumbsOverlapped)
+            {
+                return TrackThumb.Overlapped;
+            }
+        }
+
+        if (Math.Abs(lowerThumbPos + thumbHalfWidth - pointerPos) <= thumbHalfWidth)
+        {
+            return TrackThumb.Lower;
+        }
+        else if (Math.Abs(upperThumbPos + thumbHalfWidth - pointerPos) <= thumbHalfWidth)
+        {
+            return TrackThumb.Upper;
+        }
+
+        if (Math.Abs(lowerThumbPos - pointerPos) < Math.Abs(upperThumbPos - pointerPos))
+        {
+            if (pointerPos < lowerThumbPos)
+            {
+                return orient ? TrackThumb.OuterLower : TrackThumb.InnerLower;
+            }
+            else
+            {
+                return orient ? TrackThumb.InnerLower : TrackThumb.OuterLower;
+            }
+        }
+        else
+        {
+            if (pointerPos < upperThumbPos)
+            {
+                return orient ? TrackThumb.InnerUpper : TrackThumb.OuterUpper;
+            }
+            else
+            {
+                return orient ? TrackThumb.OuterUpper : TrackThumb.InnerUpper;
+            }
+        }
+    }
+
+    protected override void UpdateDataValidation<T>(AvaloniaProperty<T> property, BindingValue<T> value)
+    {
+        if (property == LowerSelectedValueProperty || property == UpperSelectedValueProperty)
+        {
+            DataValidationErrors.SetError(this, value.Error);
+        }
+    }
+
+    protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> change)
+    {
+        base.OnPropertyChanged(change);
+
+        if (change.Property == OrientationProperty)
+        {
+            UpdatePseudoClasses(change.NewValue.GetValueOrDefault<Orientation>());
+        }
+    }
+
+    private double SnapToTick(double value)
+    {
+        if (IsSnapToTickEnabled)
+        {
+            var previous = Minimum;
+            var next = Maximum;
+            var ticks = Ticks;
+
+            if (ticks != null && ticks.Count > 0)
+            {
+                foreach (var tick in ticks)
+                {
+                    if (MathUtilities.AreClose(tick, value))
+                    {
+                        return value;
+                    }
+
+                    if (MathUtilities.LessThan(tick, value) && MathUtilities.GreaterThan(tick, previous))
+                    {
+                        previous = tick;
+                    }
+                    else if (MathUtilities.GreaterThan(tick, value) && MathUtilities.LessThan(tick, next))
+                    {
+                        next = tick;
+                    }
+                }
+            }
+            else if (MathUtilities.GreaterThan(TickFrequency, 0.0))
+            {
+                previous = Minimum + Math.Round((value - Minimum) / TickFrequency) * TickFrequency;
+                next = Math.Min(Maximum, previous + TickFrequency);
+            }
+
+            value = MathUtilities.GreaterThanOrClose(value, (previous + next) * 0.5) ? next : previous;
+        }
+
+        return value;
+    }
+
+    private void UpdatePseudoClasses(Orientation o)
+    {
+        PseudoClasses.Set(":vertical", o == Orientation.Vertical);
+        PseudoClasses.Set(":horizontal", o == Orientation.Horizontal);
+    }
+}
