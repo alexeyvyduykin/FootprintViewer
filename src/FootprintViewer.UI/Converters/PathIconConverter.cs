d908b7e7a1e7f1544fa960a73e6117b2945d190a
Alexey Vyduykin <alexeyvyduykin@protonmail.com>
2023-05-31 15:33:42 +0300

    Update icons

---
+++ src/FootprintViewer.UI/Converters/PathIconConverter.cs
@@ -0,0 +1,41 @@
+﻿using Avalonia;
+using Avalonia.Data.Converters;
+using System.Globalization;
+
+namespace FootprintViewer.UI.Converters;
+
+public class PathIconConverter : IValueConverter
+{
+    public static PathIconConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string key)
+        {
+            var icon = key switch
+            {
+                "Point" => "fg-poi",
+                "Route" => "fg-route",
+                "Area" => "fg-regular-shape-o",
+                _ => default
+            };
+
+            if (icon is null)
+            {
+                return AvaloniaProperty.UnsetValue;
+            }
+
+            if (Application.Current?.Resources.TryGetResource(icon, out var resource) == true)
+            {
+                return resource;
+            }
+        }
+
+        return AvaloniaProperty.UnsetValue;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
