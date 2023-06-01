7d17af34102de3fec74b24678056edae8cb7b0a7
Alexey Vyduykin <alexeyvyduykin@protonmail.com>
2023-06-01 10:06:46 +0300

    Add PlannedScheduleOnMapSample

---
+++ samples/PlannedScheduleOnMapSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using FootprintViewer;
+using FootprintViewer.Data.Models;
+using FootprintViewer.Factories;
+using FootprintViewer.Helpers;
+using Mapsui;
+using Mapsui.Interactivity;
+using Mapsui.Interactivity.UI;
+using Mapsui.Layers;
+using ReactiveUI.Fody.Helpers;
+
+namespace PlannedScheduleOnMapSample.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public MainWindowViewModel()
+    {
+        Map = new Map();
+        Map.AddLayer(new Layer(), LayerType.WorldMap);
+
+        string path = System.IO.Path.Combine(EnvironmentHelpers.GetFullBaseDirectory(), "Assets", "world.mbtiles");
+        var resource = new MapResource("world", path);
+        Map.SetWorldMapLayer(resource);
+    }
+
+    public Map Map { get; private set; }
+
+    [Reactive]
+    public IInteractive? Interactive { get; set; }
+
+    [Reactive]
+    public string State { get; set; } = States.Default;
+}
