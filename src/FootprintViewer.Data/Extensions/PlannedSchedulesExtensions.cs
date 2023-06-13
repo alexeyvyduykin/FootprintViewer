d997b4d08f09a19a8cd71696afd0ed9327d70ebf
Alexey Vyduykin <alexeyvyduykin@protonmail.com>
2023-06-13 11:50:32 +0300

    Add observable intervals builder

--- src/FootprintViewer.Data/Extensions/PlannedSchedulesExtensions.cs
+++ src/FootprintViewer.Data/Extensions/PlannedSchedulesExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using FootprintViewer.Data.Models;
+using SpaceScience.Extensions;
+using SpaceScience.Model;
 
 namespace FootprintViewer.Data.Extensions;
 
@@ -30,5 +32,55 @@ public static class PlannedSchedulesExtensions
             .Where(s => s is CommunicationTaskResult)
             .Cast<CommunicationTaskResult>()
             .ToList();
+    }
+
+    public static Dictionary<string, Dictionary<int, List<List<(double lonDeg, double latDeg)>>>> BuildObservableIntervals(this PlannedScheduleResult ps)
+    {
+        var dict = new Dictionary<string, Dictionary<int, List<List<(double lonDeg, double latDeg)>>>>();
+
+        foreach (var satellite in ps.Satellites)
+        {
+            var nodes = satellite.NodesOnDay();
+            var name = satellite.Name;
+
+            var dict2 = new Dictionary<int, List<List<(double lonDeg, double latDeg)>>>();
+
+            for (int i = 0; i < nodes; i++)
+            {
+                var observationTasks = ps.PlannedSchedules
+                     .Where(s => s is ObservationTaskResult)
+                     .Cast<ObservationTaskResult>()
+                     .Where(s => s.Node == i)
+                     .Where(s => Equals(s.SatelliteName, name));
+
+                var orbit = satellite.ToOrbit();
+                var epoch = satellite.Epoch;
+                var period = satellite.Period;
+
+                dict2.Add(i, new());
+
+                foreach (var taskResult in observationTasks)
+                {
+                    var begin = taskResult.Interval.Begin;
+                    var duration = taskResult.Interval.Duration;
+
+                    var track = new GroundTrack(orbit);
+                    var begin0 = begin.AddSeconds(-period * i);
+
+                    var t0 = (begin0 - epoch).TotalSeconds;
+                    var t1 = t0 + duration;
+
+                    track.CalculateTrackOnTimeInterval(t0, t1, 2);
+
+                    var trackLine = track.GetTrack(i, duration, LonConverters.Default).ToCutList();
+
+                    dict2[i].AddRange(trackLine);
+                }
+            }
+
+            dict.Add(name, dict2);
+        }
+
+        return dict;
     }
 }
