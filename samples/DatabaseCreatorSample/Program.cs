5c774bf8ecc6911940502576ef4f11d2a91040e1
Alexey Vyduykin <alexeyvyduykin@protonmail.com>
2023-06-20 10:16:53 +0300

    Update

--- samples/DatabaseCreatorSample/Program.cs
+++ samples/DatabaseCreatorSample/Program.cs
@@ -2,6 +2,7 @@
 using FootprintViewer.Data.Databases;
 using FootprintViewer.Data.DbContexts;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,15 @@ namespace DatabaseCreatorSample;
 
 class Program
 {
-    static async Task Main(string[] _)
+    static async Task Main(string[] _) => await BuildDatabase();
+
+    //static void Main(string[] _) => GetAllTableNames();
+
+    private static async Task BuildDatabase()
     {
         var connectionString = ConnectionString.Build("localhost", 5432, "FootprintViewerDatabase", "postgres", "user").ToString();
 
-        var gts = await GroundTargetBuilder.CreateAsync(300);
+        var gts = await GroundTargetBuilder.CreateAsync(2000/*300*/);
         var satellites = await SatelliteBuilder.CreateAsync(5);
         var gss = await GroundStationBuilder.CreateDefaultAsync();
 
@@ -41,6 +46,28 @@ class Program
         db.UserGeometries.AddRange(tempUserGeometry);
 
         Console.WriteLine("Database build");
+    }
+
+    private static void GetAllTableNames()
+    {
+        var connectionString = ConnectionString.Build("localhost", 5432, "FootprintViewerDatabase", "postgres", "user").ToString();
+
+        using var connection = new NpgsqlConnection(connectionString);
+
+        connection.Open();
+
+        using var command = new NpgsqlCommand("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE';", connection);
+        using var reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            var tableName = reader.GetString(0);
+
+            if (DbHelpers.TryValidateContext(DbKeys.PlannedSchedules, () => new PlannedScheduleDbContext(tableName, connectionString)) == true)
+            {
+                Console.WriteLine($"For {DbKeys.PlannedSchedules} exist table: {tableName}");
+            }
+        }
     }
 
     private static void CreateDb(IEnumerable<DbContext> dbs)
